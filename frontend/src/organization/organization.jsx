c1ba7166bb13f003eb968f74d2fc75ce604f4ac1
Sophia Peckner <sophiapeckner@gmail.com>
2024-03-02 20:33:16 -0800

    load list of organizations

--- frontend/src/organization/organization.jsx
+++ frontend/src/organization/organization.jsx
@@ -1,22 +1,27 @@
-import "./organization.css"
+import "./organization.css";
 
-export default function Organization () {
-    return (
-        <div className="org">
-            <div className="orgImage">
-                <img src="/logo192.png"/>
-            </div>
-            <div className="orgInfo">
-                <div className="orgTitle">
-                    Org Name
-                </div>
-                <div className="orgDesc">
-                    Description of Organization
-                </div>
-                <div className="orgRating">
-                </div>
-            </div>
-            
-        </div>
-    )
+const apiUrl = "http://localhost:3001/calpoly/organizations";
+let orgList = [];
+let response = await fetch(apiUrl);
+let myjson = await response.json();
+console.log(response);
+let orgs = myjson["value"];
+for (let i = 0; i < orgs.length; i++) {
+  orgList.push(orgs[i]["Name"]);
+}
+
+export default function Organization() {
+  console.log(orgList);
+  return (
+    <div className="org">
+      <div className="orgImage">
+        <img src="/logo192.png" />
+      </div>
+      <div className="orgInfo">
+        <div className="orgTitle">Org Name</div>
+        <div className="orgDesc">Description of Organization</div>
+        <div className="orgRating"></div>
+      </div>
+    </div>
+  );
 }
