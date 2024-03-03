5eaed17295942b7b9a3b1a304ebc85d904b04a4d
Sophia Peckner <sophiapeckner@gmail.com>
2024-03-02 20:35:27 -0800

    add routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import NavBar from './navbar/navbar';
-import Organizations from './organizations/organizations'
-import { Route, Routes } from 'react-router-dom';
-import CreateAccount from './signUp/createAccount';
-import Login from './login/login';
+import React from "react";
+import NavBar from "./navbar/navbar";
+import { Route, Routes } from "react-router-dom";
+import CreateAccount from "./signUp/createAccount";
+import Login from "./login/login";
+import Organizations from "./organizations/organizations";
 
 export const Routes1 = () => {
-    return (
-      <div>
-        <NavBar/>
-        <Routes>
-          <Route exact path="/Organizations" element={Organizations()} />
-          <Route exact path="/CreateAccount" element={CreateAccount()}/>
-          <Route exact path='/Login' element={Login()}/>
-        </Routes>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route exact path="/Organizations" element={Organizations()} />
+        <Route exact path="/CreateAccount" element={CreateAccount()} />
+        <Route exact path="/Login" element={Login()} />
+      </Routes>
+    </div>
+  );
+};
