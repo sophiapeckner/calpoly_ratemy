82b2ead104943c63ecf16594cda07ef9748cbc9f
Rachana <rachanaraju03@gmail.com>
2024-03-03 10:43:15 -0800

    events page and event component

---
+++ frontend/src/events/events.jsx
@@ -0,0 +1,34 @@
+import "./events.css"
+import Event from "../event/event"
+
+export default function Events() {
+    return (
+        <div className="eventsPage">
+            <div className="events">
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+                <Event />
+            </div>
+        </div>
+        
+    )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import CreateAccount from "./signUp/createAccount";
 import Login from "./login/login";
 import Organizations from "./organizations/organizations";
+import Events from "./events/events";
 
 export const Routes1 = () => {
   return (
@@ -11,6 +12,7 @@ export const Routes1 = () => {
       <NavBar />
       <Routes>
         <Route exact path="/Organizations" element={Organizations()} />
+        <Route exact path="/Events" element={Events()}/>
         <Route exact path="/CreateAccount" element={CreateAccount()} />
         <Route exact path="/Login" element={Login()} />
       </Routes>
