ab62208d9b77b1ed3c4b636dc2d988033f4aaddc
Rachana <rachanaraju03@gmail.com>
2024-03-03 14:31:34 -0800

    Merge branch 'main'

--- frontend/src/Pages/commentPage.jsx
+++ frontend/src/Pages/commentPage.jsx
@@ -1,6 +1,16 @@
-import "./comment.css"
+
+import "./commentPage.css"
+import StarRating from "../components/starRating"
+import { useState } from "react"
 
 export default function CommentPage () {
+    const [rate,setRate]=useState()
+    const [commentValue,setCommentValue]=useState()
+
+    const handleSubmit=(e)=>{
+        console.log("rating: "+rate)
+        console.log("comment: "+commentValue)
+    }
     return (
         <div className="comment">
             <div className="commentBody">
@@ -8,13 +18,16 @@ export default function CommentPage () {
                     Name of Org
                 </div>
                 <div className="commentDesc">
-                    <form className="commentForm">
-                        <textarea className="commentText" type="text" placeholder="Enter comment here..."></textarea>
+                    <form className="commentForm" onSubmit={handleSubmit}>
+                        <label>Enter your rating 1-5</label>
+                        <input className="commentStar" type="number" placeholder="num" onChange={(e)=>setRate(e.target.value)}/>
+                        <textarea className="commentText" type="text" placeholder="Enter comment here..." onChange={(e)=>setCommentValue(e.target.value)}></textarea>
                         <button className="commentButton">Submit</button>    
                     </form>
                     
                 </div>
             </div>
+        
             
     
         </div>
